f65dd805bb48809ae0154eaff443d168bc52e7a9
scrappy <scrappycoco902@gmail.com>
2023-10-30 13:14:20 +0530

    progressing

--- flex/models.py
+++ flex/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel, PageChooserPanel
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 
 # Create your models here.
 
@@ -11,7 +11,7 @@ class FlexPage(Page):
     subtitle = models.CharField(max_length=100, blank=True, null=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('subtitle')
+        FieldPanel('subtitle'),
     ]
 
     
